b0c179689a22be4a6ee39d06a41171c93a208d78
Eric Régnier <utopman@gmail.com>
2019-02-24 10:50:46 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+Bin packing 2D Optimizer
+==================
+
+Dataset is preprocessed with quick shelf algorithm to optimize initial position with a restricted width, then this optimizer will try to move all possible pieces from
+the most large ones to smaller one at every possible available position above current position.
+
+It is initially intended to move all possible pieces at bottom to the most top position to let as possible large free spaces at the bottom of the pack, but it
+sometimes finds more optimal dispositions of the pack that the Shelve algorithm.
+
+This algorithm is time expansive and has a great complexity. it should be used for small data set to remain practicable or user would have to be patient.
+It takes about 1 second to process a dataset of about 40 pieces.
+
+Usage:
+
+```javascript
+
+  import Optimizer from 'bin-pack-2d-enhencer'
+
+  let pack = [
+    {w: 10, h: 15},
+    {w: 20, h: 25},
+    {w: 30, h: 35},
+  ]
+
+  pack = new Optimizer(pack, 140, 100000).optimize()
+  /*
+  pack items have now a w(width) and h(height) attributes
+  [
+    {w: 10, h: 15},
+    {w: 20, h: 25},
+    {w: 30, h: 35},
+  ]
+  */
+```
---
+++ index.js
@@ -0,0 +1,122 @@
+var ShelfPack = require('@mapbox/shelf-pack').default
+
+export default class Optimizer {
+  constructor (pieces, width, height) {
+    this.pieces = pieces
+    this.width = width
+    this.height = height
+  }
+  optimize () {
+    this.pieces = this.preprocess(this.pieces)
+    this.height = this.computeHeight(this.pieces)
+
+    let hasChange = true
+    while (hasChange) {
+      const orderedPieces = this.rankPieces().map(p => {
+        delete p._opti
+        return p
+      })
+      hasChange = this.tryOptimize(orderedPieces)
+    }
+
+    return this.pieces.map(p => {
+      delete p._opti
+      return p
+    })
+  }
+  preprocess (pieces) {
+    const packer = new ShelfPack(this.width, this.height)
+    const optimizedPieces = packer.pack(pieces.map(p => {
+      return {
+        w: parseInt(p.w),
+        h: parseInt(p.h)
+      }
+    }))
+    packer.clear()
+    return optimizedPieces
+  }
+  computeHeight (pieces) {
+    let max = 0
+    let h = 0
+    for (let piece of pieces) {
+      h = piece.y + piece.h
+      max = h > max ? h : max;
+    }
+    return max
+  }
+  getIgnorePoints (pieces) {
+    const cache = {}
+    for (const piece of pieces) {
+      const rectangle = this.toCompareRect(piece)
+      for (let x=rectangle.left; x< rectangle.right; x++) {
+        for (let y=rectangle.top; y< rectangle.bottom; y++) {
+          cache[x+'-'+y] = true
+        }
+      }
+    }
+    return cache
+  }
+  tryOptimize (orderedPieces) {
+    const ignorePoints = this.getIgnorePoints(orderedPieces)
+    for (const piece of orderedPieces) {
+      if (!piece._opti) {
+        let maxWidth = this.width - piece.w + 1
+        let maxHeight = this.height - piece.h + 1
+        for (let y=0; y< maxHeight; y++) {
+          for (let x=0; x< maxWidth; x++) {
+            if (!ignorePoints[x + '-' + y]) {
+              const testPiece = {
+                x: x,
+                y: y,
+                w: piece.w,
+                h: piece.h
+              }
+              const hasBetterScore = y < piece.y
+              const respectConstraints = x + piece.w <= this.width
+              const hasSpace = !this.intersects(testPiece, this.pieces)
+              if(hasSpace && hasBetterScore && respectConstraints) {
+                piece.x = x
+                piece.y = y
+                piece._opti = true
+                return true
+              }
+            }
+          }
+        }
+      }
+    }
+    return false
+  }
+  rankPieces () {
+    return this.pieces.sort((a, b) => {
+      return ((a.w * a.h) - (b.w * b.h))
+    })
+  }
+  toCompareRect (rect) {
+    return {
+      left : rect.x,
+      right : rect.x + rect.w,
+      top : rect.y,
+      bottom : rect.y + rect.h
+    }
+  }
+  intersectRect(r1, r2) {
+    return !(
+      r2.left >= r1.right ||
+      r2.right <= r1.left ||
+      r2.top >= r1.bottom ||
+      r2.bottom <= r1.top
+    )
+  }
+  intersects (p, pieces) {
+    for (const piece of pieces) {
+      if(this.intersectRect(
+        this.toCompareRect(p),
+        this.toCompareRect(piece),
+      )) {
+        return true
+      }
+    }
+    return false
+  }
+}
